14d19b06aeffa082c0973c7a2dda9359c65ef8ce
Gesiel Oliveira <gesiel.br@gmail.com>
2026-02-16 17:55:59 -0300

    initial project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { CharactersPage } from './features/characters/pages/characters/characters.page';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'characters',
+    loadChildren: () =>
+      import('./features/characters/pages/characters/characters.routes').then(
+        (m) => m.CHARACTERS_ROUTES,
+      ),
+  },
+];
---
+++ src/app/features/characters/pages/characters/characters.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CharactersPage } from './characters.page';
+
+export const CHARACTERS_ROUTES: Routes = [
+  {
+    path: '',
+    component: CharactersPage,
+  },
+];
